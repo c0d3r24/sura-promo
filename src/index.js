b1064f3073ea2578f3b7d8072da2471ff7578619
Junaid Azhar Shaikh <junaidazharshaikh@gmail.com>
2018-02-28 17:38:51 -0500

    initial routing fixed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import {Router, browserHistory} from 'react-router';
+import Routes from './routes'
 
-ReactDOM.render(<div>
-    <h1> Hello World</h1>
-</div>, document.getElementById('root'));
+ReactDOM.render(
+    <Router 
+    history={browserHistory}
+    routes={Routes} />, document.getElementById('root'));
 registerServiceWorker();
