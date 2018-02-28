009f5c3eb5aa0cacd16a73df1a6fb9df99b2395e
Junaid Azhar Shaikh <junaidazharshaikh@gmail.com>
2018-02-28 18:29:28 -0500

    added materia-ui and tapsupport

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,16 +1,29 @@
 import React ,{ Component } from 'react';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import RaisedButton from 'material-ui/RaisedButton';
+/**
+ * injecting TapEvent here.
+ * this will help to enhance mobile interaction.
+*/
+injectTapEventPlugin()
 class Template extends Component {
     render(){
         return(
-            <div>
+            <MuiThemeProvider>
+                <div>
                 <header>
                     <h1>Sura</h1>
+                    <RaisedButton
+                    label={'Tap this button'}
+                    onTouchTap={() => {console.log('Hello, I workd')}}/>
                 </header>
                 <main>
                     {this.props.children}
                 </main>
             </div>
+            </MuiThemeProvider>
+            
         );
     }
 }
